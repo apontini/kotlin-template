11e78f9451a063f903d39023f24bed04dd21d8ba
apontini <alberto.pontini@gmail.com>
2021-09-09 21:31:22 +0200

    add db name as env variable

--- src/main/kotlin/me/apontini/ktemplate/user/infrastructure/UserMongoRepository.kt
+++ src/main/kotlin/me/apontini/ktemplate/user/infrastructure/UserMongoRepository.kt
@@ -13,26 +13,26 @@ class UserMongoRepository : UserRepository, KoinComponent {
     private val client by inject<MongoClient>()
 
     override fun save(user: User): User {
-        client.getDatabase("kchat")
+        client.getDatabase(System.getenv("DB_NAME"))
             .getCollection("user", User::class.java)
             .replaceOneById(user.id, user, ReplaceOptions().upsert(true))
         return user
     }
 
     override fun delete(user: User) {
-        client.getDatabase("kchat")
+        client.getDatabase(System.getenv("DB_NAME"))
             .getCollection("user", User::class.java)
             .deleteOne(User::id eq user.id)
     }
 
     override fun findByEmail(email: String): User? {
-        return client.getDatabase("kchat")
+        return client.getDatabase(System.getenv("DB_NAME"))
             .getCollection("user", User::class.java)
             .findOne(User::email eq email)
     }
 
     override fun findById(id: String): User? {
-        return client.getDatabase("kchat")
+        return client.getDatabase(System.getenv("DB_NAME"))
             .getCollection("user", User::class.java)
             .findOne(User::id eq id)
     }
